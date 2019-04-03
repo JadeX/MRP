1030f6289d97c304a0ba55ec481e22f41c78fd42
Xeevis <Xeevis@users.noreply.github.com>
2019-04-03 17:13:13 +0200

    Coding style fix

--- MRP.Tests/TestCryptography.cs
+++ MRP.Tests/TestCryptography.cs
@@ -7,25 +7,25 @@ namespace MRP.Tests
 {
     public class TestCryptography
     {
-        private const string _AESIVVectorHex = "09-18-29-E9-98-18-6D-9F-B0-78-72-2E-0B-91-44-06";
-        private const string _authenticationCodeHex = "B5-75-94-6C-75-DC-A6-5C-E0-DA-8A-C2-AC-F4-73-72-34-09-68-99-53-6E-88-05-51-CA-D1-AE-99-DE-F3-6A";
-        private const string _authenticationKeyHex = "5B-DF-74-9A-16-63-DF-20-6A-1E-9E-36-03-96-33-75-92-FD-D8-2F-66-05-CF-3A-F8-D4-D4-54-6B-64-05-06";
-        private const string _encryptedTextHex = "01-EC-4D-BE-B1-04-CD-38-E9-0A-4E-CC-C5-C5-35-9C-D0-AA-D8-AF";
-        private const string _encryptionKeyHex = "00-06-29-E3-9E-79-F3-F5-2B-05-D8-58-72-40-C3-81-CA-14-A0-EC-17-27-A9-5A-FA-D4-80-EB-D5-6E-1C-40";
-        private const string _openTextHex = "01-02-03-04-05-06-07-08-09-0A-0B-0C-0D-0E-0F-10-11-12-13-14";
-        private const string _privateEncryptionKeyHex = "DE-B5-81-AB-EC-C4-A5-A5-5D-C7-6C-08-A9-75-49-62-BD-A0-54-10-E1-A3-0D-5E-99-05-AD-FA-65-6C-F2-C9";
-        private const string _secretKey = "bRtFEufmEgrJyhai6ltDSV9svtpN3Jb/5oWBBYhDJ30=";
-        private const string _secretKeyHex = "6D-1B-45-12-E7-E6-12-0A-C9-CA-16-A2-EA-5B-43-49-5F-6C-BE-DA-4D-DC-96-FF-E6-85-81-05-88-43-27-7D";
-        private const string _sequenceHex = "01-02-03-04-05-06-07-08-09-0A-0B-0C-0D-0E-0F-10-11-12-13-14";
-        private const string _variantKeyHex = "1F-5A-C7-7E-D3-0C-C0-A5-F7-5B-B0-35-FF-05-66-A5-0D-B2-12-7A-AB-32-D8-62-4E-0D-A4-D4-18-6E-7F-2F";
+        private const string AesIvVectorHex = "09-18-29-E9-98-18-6D-9F-B0-78-72-2E-0B-91-44-06";
+        private const string AuthenticationCodeHex = "B5-75-94-6C-75-DC-A6-5C-E0-DA-8A-C2-AC-F4-73-72-34-09-68-99-53-6E-88-05-51-CA-D1-AE-99-DE-F3-6A";
+        private const string AuthenticationKeyHex = "5B-DF-74-9A-16-63-DF-20-6A-1E-9E-36-03-96-33-75-92-FD-D8-2F-66-05-CF-3A-F8-D4-D4-54-6B-64-05-06";
+        private const string EncryptedTextHex = "01-EC-4D-BE-B1-04-CD-38-E9-0A-4E-CC-C5-C5-35-9C-D0-AA-D8-AF";
+        private const string EncryptionKeyHex = "00-06-29-E3-9E-79-F3-F5-2B-05-D8-58-72-40-C3-81-CA-14-A0-EC-17-27-A9-5A-FA-D4-80-EB-D5-6E-1C-40";
+        private const string OpenTextHex = "01-02-03-04-05-06-07-08-09-0A-0B-0C-0D-0E-0F-10-11-12-13-14";
+        private const string PrivateEncryptionKeyHex = "DE-B5-81-AB-EC-C4-A5-A5-5D-C7-6C-08-A9-75-49-62-BD-A0-54-10-E1-A3-0D-5E-99-05-AD-FA-65-6C-F2-C9";
+        private const string SecretKey = "bRtFEufmEgrJyhai6ltDSV9svtpN3Jb/5oWBBYhDJ30=";
+        private const string SecretKeyHex = "6D-1B-45-12-E7-E6-12-0A-C9-CA-16-A2-EA-5B-43-49-5F-6C-BE-DA-4D-DC-96-FF-E6-85-81-05-88-43-27-7D";
+        private const string SequenceHex = "01-02-03-04-05-06-07-08-09-0A-0B-0C-0D-0E-0F-10-11-12-13-14";
+        private const string VariantKeyHex = "1F-5A-C7-7E-D3-0C-C0-A5-F7-5B-B0-35-FF-05-66-A5-0D-B2-12-7A-AB-32-D8-62-4E-0D-A4-D4-18-6E-7F-2F";
         private readonly Cryptography _crypto;
 
-        public TestCryptography() => _crypto = new Cryptography(_secretKey, Convert.ToBase64String(ConvertHexToByteArray(_variantKeyHex)));
+        public TestCryptography() => _crypto = new Cryptography(SecretKey, Convert.ToBase64String(ConvertHexToByteArray(VariantKeyHex)));
 
         [Fact]
         public void AESCTREncryption()
         {
-            var variantKey = ConvertHexToByteArray(_variantKeyHex);
+            var variantKey = ConvertHexToByteArray(VariantKeyHex);
 
             byte[] iV;
 
@@ -33,16 +33,16 @@ namespace MRP.Tests
             {
                 iV = sha.ComputeHash(variantKey).Take(16).ToArray();
 
-                Assert.Equal(_AESIVVectorHex, BitConverter.ToString(iV));
+                Assert.Equal(AesIvVectorHex, BitConverter.ToString(iV));
             }
 
-            var inputBytes = ConvertHexToByteArray(_openTextHex);
+            var inputBytes = ConvertHexToByteArray(OpenTextHex);
 
-            var keyBytes = _crypto.Hmac_Sha256(ConvertHexToByteArray(_privateEncryptionKeyHex), ConvertHexToByteArray(_variantKeyHex));
+            var keyBytes = _crypto.Hmac_Sha256(ConvertHexToByteArray(PrivateEncryptionKeyHex), ConvertHexToByteArray(VariantKeyHex));
 
             var encryptedBytes = _crypto.EncryptData(inputBytes);
 
-            Assert.Equal(_encryptedTextHex, BitConverter.ToString(encryptedBytes));
+            Assert.Equal(EncryptedTextHex, BitConverter.ToString(encryptedBytes));
 
             var decryptedBytes = _crypto.EncryptData(encryptedBytes);
 
@@ -54,22 +54,22 @@ namespace MRP.Tests
         public void Authentication() =>
 
             // Authentication code
-            Assert.Equal(_authenticationCodeHex, BitConverter.ToString(_crypto.Hmac_Sha256(ConvertHexToByteArray(_authenticationKeyHex), ConvertHexToByteArray(_sequenceHex))));
+            Assert.Equal(AuthenticationCodeHex, BitConverter.ToString(_crypto.Hmac_Sha256(ConvertHexToByteArray(AuthenticationKeyHex), ConvertHexToByteArray(SequenceHex))));
 
         [Fact]
         public void VerifyCryptoHexes()
         {
             // Secret key
-            Assert.Equal(_secretKeyHex, BitConverter.ToString(Convert.FromBase64String(_secretKey)));
+            Assert.Equal(SecretKeyHex, BitConverter.ToString(Convert.FromBase64String(SecretKey)));
 
             // Private encryption key
-            Assert.Equal(_privateEncryptionKeyHex, BitConverter.ToString(_crypto.PrivateEncryptionKey));
+            Assert.Equal(PrivateEncryptionKeyHex, BitConverter.ToString(_crypto.PrivateEncryptionKey));
 
             // Authentication key
-            Assert.Equal(_authenticationKeyHex, BitConverter.ToString(_crypto.AuthenticationKey));
+            Assert.Equal(AuthenticationKeyHex, BitConverter.ToString(_crypto.AuthenticationKey));
 
             // Encryption key
-            Assert.Equal(_encryptionKeyHex, BitConverter.ToString(_crypto.Hmac_Sha256(ConvertHexToByteArray(_privateEncryptionKeyHex), ConvertHexToByteArray(_variantKeyHex))));
+            Assert.Equal(EncryptionKeyHex, BitConverter.ToString(_crypto.Hmac_Sha256(ConvertHexToByteArray(PrivateEncryptionKeyHex), ConvertHexToByteArray(VariantKeyHex))));
         }
 
         private byte[] ConvertHexToByteArray(string hex) => Array.ConvertAll(hex.Split('-'), s => Convert.ToByte(s, 16));

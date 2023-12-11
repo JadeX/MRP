c441ed99dbeec3754454c676ae2c5ead65863612
Xeevis <Xeevis@users.noreply.github.com>
2023-12-11 16:06:43 +0100

    🚨 CA1707: Identifiers should not contain underscores
    https://learn.microsoft.com/en-us/dotnet/fundamentals/code-analysis/quality-rules/ca1707

--- src/Xml/Datasets/MrpReplacement.cs
+++ src/Xml/Datasets/MrpReplacement.cs
@@ -9,17 +9,17 @@ public class MrpReplacement
     public float Cislo { get; set; }
 
     [XmlElement("cislo_z")]
-    public float Cislo_Z { get; set; }
+    public float CisloZ { get; set; }
 
     [XmlElement("kod")]
     public string Kod { get; set; }
 
     [XmlElement("kod_z")]
-    public string Kod_Z { get; set; }
+    public string KodZ { get; set; }
 
     [XmlElement("kod1")]
     public string Kod1 { get; set; }
 
     [XmlElement("kod1_z")]
-    public string Kod1_Z { get; set; }
+    public string Kod1Z { get; set; }
 }
